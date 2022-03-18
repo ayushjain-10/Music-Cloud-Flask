84e90020069602275f064e8fde0937f3798be09b
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-03-18 05:40:06 -0700

    rename

--- musiccloud/posts/routes.py
+++ musiccloud/posts/routes.py
@@ -1,9 +1,9 @@
 from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import current_user, login_required
-from flaskblog import db
-from flaskblog.models import Post
-from flaskblog.posts.forms import PostForm
+from musiccloud import db
+from musiccloud.models import Post
+from musiccloud.posts.forms import PostForm
 
 posts = Blueprint('posts', __name__)
 
